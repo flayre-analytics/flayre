a7c682bed01f41521c208f9c0caad9f7d55c6297
William Brawner <me@wbrawner.com>
2020-09-04 14:02:57 -0700

    Convert server code from Kotlin/Ktor to Java/Spring Boot
    In the long-term, I'm not confident that Kotlin will replace Java on the
    backend nor that Ktor will replace Spring Boot as the most popular
    JVM language or framework, respectively. I don't really have any desire
    to be on the bleeding edge of tech or go off the beaten path for this so
    I'd like to keep with the industry standards so that it's easier for me
    to work on over time.

---
+++ server/src/main/java/com/wbrawner/flayre/server/FlayreApplication.java
@@ -0,0 +1,13 @@
+package com.wbrawner.flayre.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlayreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlayreApplication.class, args);
+    }
+
+}
---
+++ server/src/test/java/com/wbrawner/flayre/server/FlayreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wbrawner.flayre.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlayreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
