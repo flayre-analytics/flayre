ce4d00a788e814012c2debb602769f86cb19cb9b
William Brawner <me@wbrawner.com>
2020-09-05 11:23:39 -0700

    Delete events older than 90 days once per day.
    Signed-off-by: William Brawner <me@wbrawner.com>

--- server/src/main/java/com/wbrawner/flayre/server/FlayreApplication.java
+++ server/src/main/java/com/wbrawner/flayre/server/FlayreApplication.java
@@ -2,12 +2,13 @@ package com.wbrawner.flayre.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class FlayreApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(FlayreApplication.class, args);
     }
-
 }
---
+++ server/src/main/java/com/wbrawner/flayre/server/events/EventScheduler.java
@@ -0,0 +1,32 @@
+package com.wbrawner.flayre.server.events;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.Calendar;
+import java.util.TimeZone;
+
+/**
+ * Responsible for deleting events older than a given date
+ */
+@Component
+public class EventScheduler {
+    private static final Logger logger = LoggerFactory.getLogger(EventScheduler.class);
+    private final EventRepository eventRepository;
+
+    @Autowired
+    public EventScheduler(EventRepository eventRepository) {
+        this.eventRepository = eventRepository;
+    }
+
+    @Scheduled(cron = "0 0 * * *")
+    public void deleteOldEvents() {
+        Calendar calendar = Calendar.getInstance(TimeZone.getTimeZone("UTC"));
+        calendar.add(Calendar.DATE, -90);
+        int deletedEvents = eventRepository.deleteEvents(calendar.getTime());
+        logger.info("Deleted {} events older than 90 days", deletedEvents);
+    }
+}
