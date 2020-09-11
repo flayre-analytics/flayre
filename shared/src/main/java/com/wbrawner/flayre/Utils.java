80c8198edd855f8f009da388400476edb5640edd
William Brawner <me@wbrawner.com>
2020-09-11 06:55:19 -0700

    Use Java 8 for shared code
    This is needed in order for it to be usable on Android
    
    Signed-off-by: William Brawner <me@wbrawner.com>

--- shared/src/main/java/com/wbrawner/flayre/Utils.java
+++ shared/src/main/java/com/wbrawner/flayre/Utils.java
@@ -6,8 +6,8 @@ public final class Utils {
     private static final String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
     public static String randomId(int length) {
-        final var id = new StringBuilder();
-        final var random = new Random();
+        final StringBuilder id = new StringBuilder();
+        final Random random = new Random();
         while (id.length() < length) {
             id.append(characters.charAt(random.nextInt(characters.length())));
         }
